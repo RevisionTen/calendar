bcafc6915b745db4957a8b92e7691a053c4cbb75
Cornelius Kahns <67231872+TAR5@users.noreply.github.com>
2023-09-12 10:53:44 +0200

    Add optional event start and end date

--- Handler/EventCreateHandler.php
+++ Handler/EventCreateHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RevisionTen\Calendar\Handler;
 
+use DateTime;
 use ReflectionObject;
 use ReflectionProperty;
 use RevisionTen\Calendar\Event\EventCreateEvent;
@@ -31,6 +32,28 @@ final class EventCreateHandler implements HandlerInterface
         $reflect = new ReflectionObject($aggregate);
         foreach ($reflect->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
             $propertyName = $property->getName();
+
+            if ('startDate' === $propertyName) {
+                if (!empty($payload['startDate'])) {
+                    $startDate = $payload['startDate'];
+                    $aggregate->startDate = new DateTime();
+                    $aggregate->startDate->setTimestamp($startDate);
+                } else {
+                    $aggregate->startDate = null;
+                }
+                continue;
+            }
+            if ('endDate' === $propertyName) {
+                if (!empty($payload['endDate'])) {
+                    $endDate = $payload['endDate'];
+                    $aggregate->endDate = new DateTime();
+                    $aggregate->endDate->setTimestamp($endDate);
+                } else {
+                    $aggregate->endDate = null;
+                }
+                continue;
+            }
+
             if (array_key_exists($propertyName, $payload)) {
                 $aggregate->{$propertyName} = $payload[$propertyName];
             }
--- Handler/EventEditHandler.php
+++ Handler/EventEditHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RevisionTen\Calendar\Handler;
 
+use DateTime;
 use ReflectionObject;
 use ReflectionProperty;
 use RevisionTen\Calendar\Event\EventEditEvent;
@@ -30,6 +31,28 @@ final class EventEditHandler implements HandlerInterface
         $reflect = new ReflectionObject($aggregate);
         foreach ($reflect->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
             $propertyName = $property->getName();
+
+            if ('startDate' === $propertyName) {
+                if (!empty($payload['startDate'])) {
+                    $startDate = $payload['startDate'];
+                    $aggregate->startDate = new DateTime();
+                    $aggregate->startDate->setTimestamp($startDate);
+                } else {
+                    $aggregate->startDate = null;
+                }
+                continue;
+            }
+            if ('endDate' === $propertyName) {
+                if (!empty($payload['endDate'])) {
+                    $endDate = $payload['endDate'];
+                    $aggregate->endDate = new DateTime();
+                    $aggregate->endDate->setTimestamp($endDate);
+                } else {
+                    $aggregate->endDate = null;
+                }
+                continue;
+            }
+
             if (array_key_exists($propertyName, $payload)) {
                 $aggregate->{$propertyName} = $payload[$propertyName];
             }
